262dfb43bb844bd5b4dcf0d94f300a5f7d597339
andrecolacoml <andrecolacoml@gmail.com>
2024-08-15 17:08:09 -0300

    Upload 1.0.4
    Upload

---
+++ SharedMedia_MyMedia/MyMedia.lua
@@ -0,0 +1,91 @@
+local LSM = LibStub("LibSharedMedia-3.0") 
+
+-- ----- 
+-- BACKGROUND 
+-- ----- 
+LSM:Register("background", "arrow_glow", [[Interface\Addons\SharedMedia_MyMedia\background\arrow_glow.tga]]) 
+LSM:Register("background", "glowTex", [[Interface\Addons\SharedMedia_MyMedia\background\glowTex.tga]]) 
+LSM:Register("background", "Minimalist", [[Interface\Addons\SharedMedia_MyMedia\background\Minimalist.tga]]) 
+LSM:Register("background", "normTex", [[Interface\Addons\SharedMedia_MyMedia\background\normTex.tga]]) 
+LSM:Register("background", "normTex2", [[Interface\Addons\SharedMedia_MyMedia\background\normTex2.tga]]) 
+LSM:Register("background", "Smooth", [[Interface\Addons\SharedMedia_MyMedia\background\Smooth.tga]]) 
+
+-- ----- 
+--  BORDER 
+-- ---- 
+LSM:Register("border", "Caith", [[Interface\Addons\SharedMedia_MyMedia\border\Caith.tga]]) 
+LSM:Register("border", "HalBorder", [[Interface\Addons\SharedMedia_MyMedia\border\HalBorder.tga]]) 
+LSM:Register("border", "krsnik", [[Interface\Addons\SharedMedia_MyMedia\border\krsnik.tga]]) 
+LSM:Register("border", "Roth", [[Interface\Addons\SharedMedia_MyMedia\border\Roth.tga]]) 
+LSM:Register("border", "siana", [[Interface\Addons\SharedMedia_MyMedia\border\siana.tga]]) 
+
+-- -----
+--   FONT
+-- -----
+LSM:Register("font", "ArchivoNarrow-Bold", [[Interface\Addons\SharedMedia_MyMedia\font\ArchivoNarrow-Bold.ttf]]) 
+LSM:Register("font", "HoogeMerfin", [[Interface\Addons\SharedMedia_MyMedia\font\HoogeMerfin.ttf]]) 
+
+-- -----
+--   SOUND
+-- -----
+LSM:Register("sound", "1", [[Interface\Addons\SharedMedia_MyMedia\sound\1.mp3]]) 
+LSM:Register("sound", "2", [[Interface\Addons\SharedMedia_MyMedia\sound\2.mp3]]) 
+LSM:Register("sound", "3", [[Interface\Addons\SharedMedia_MyMedia\sound\3.mp3]]) 
+LSM:Register("sound", "4", [[Interface\Addons\SharedMedia_MyMedia\sound\4.mp3]]) 
+LSM:Register("sound", "5", [[Interface\Addons\SharedMedia_MyMedia\sound\5.mp3]]) 
+LSM:Register("sound", "Alarm", [[Interface\Addons\SharedMedia_MyMedia\sound\Alarm.mp3]]) 
+LSM:Register("sound", "AlertBell", [[Interface\Addons\SharedMedia_MyMedia\sound\AlertBell.mp3]]) 
+LSM:Register("sound", "BeaconMissing", [[Interface\Addons\SharedMedia_MyMedia\sound\BeaconMissing.mp3]]) 
+LSM:Register("sound", "Decimation", [[Interface\Addons\SharedMedia_MyMedia\sound\Decimation.mp3]]) 
+LSM:Register("sound", "DoNotRelease", [[Interface\Addons\SharedMedia_MyMedia\sound\DoNotRelease.wav]]) 
+LSM:Register("sound", "DrainSoul", [[Interface\Addons\SharedMedia_MyMedia\sound\DrainSoul.mp3]]) 
+LSM:Register("sound", "FollowMark", [[Interface\Addons\SharedMedia_MyMedia\sound\FollowMark.wav]]) 
+LSM:Register("sound", "GoToLeftSide", [[Interface\Addons\SharedMedia_MyMedia\sound\GoToLeftSide.wav]]) 
+LSM:Register("sound", "GoToMiddleSide", [[Interface\Addons\SharedMedia_MyMedia\sound\GoToMiddleSide.wav]]) 
+LSM:Register("sound", "GoToRightSide", [[Interface\Addons\SharedMedia_MyMedia\sound\GoToRightSide.wav]]) 
+LSM:Register("sound", "GoToShambling", [[Interface\Addons\SharedMedia_MyMedia\sound\GoToShambling.wav]]) 
+LSM:Register("sound", "Green", [[Interface\Addons\SharedMedia_MyMedia\sound\Green.wav]]) 
+LSM:Register("sound", "Hysteria", [[Interface\Addons\SharedMedia_MyMedia\sound\Hysteria.wav]]) 
+LSM:Register("sound", "IdiotSalle", [[Interface\Addons\SharedMedia_MyMedia\sound\IdiotSalle.wav]]) 
+LSM:Register("sound", "Interrupt", [[Interface\Addons\SharedMedia_MyMedia\sound\Interrupt.wav]]) 
+LSM:Register("sound", "MoltenCore", [[Interface\Addons\SharedMedia_MyMedia\sound\MoltenCore.mp3]]) 
+LSM:Register("sound", "NoLifeTap", [[Interface\Addons\SharedMedia_MyMedia\sound\NoLifeTap.mp3]]) 
+LSM:Register("sound", "PestilenceMiss", [[Interface\Addons\SharedMedia_MyMedia\sound\PestilenceMiss.mp3]]) 
+LSM:Register("sound", "piratochniki_sadge", [[Interface\Addons\SharedMedia_MyMedia\sound\piratochniki_sadge.mp3]]) 
+LSM:Register("sound", "PowerInfusion", [[Interface\Addons\SharedMedia_MyMedia\sound\PowerInfusion.wav]]) 
+LSM:Register("sound", "PowerInfusionOnYou", [[Interface\Addons\SharedMedia_MyMedia\sound\PowerInfusionOnYou.wav]]) 
+LSM:Register("sound", "RefreshFlask", [[Interface\Addons\SharedMedia_MyMedia\sound\RefreshFlask.wav]]) 
+LSM:Register("sound", "RightSide", [[Interface\Addons\SharedMedia_MyMedia\sound\RightSide.wav]]) 
+LSM:Register("sound", "RunOut", [[Interface\Addons\SharedMedia_MyMedia\sound\RunOut.wav]]) 
+LSM:Register("sound", "SacredShieldMissing", [[Interface\Addons\SharedMedia_MyMedia\sound\SacredShieldMissing.mp3]]) 
+LSM:Register("sound", "SpreadOut", [[Interface\Addons\SharedMedia_MyMedia\sound\SpreadOut.wav]]) 
+LSM:Register("sound", "StopCleave", [[Interface\Addons\SharedMedia_MyMedia\sound\StopCleave.wav]]) 
+LSM:Register("sound", "SwitchTanks", [[Interface\Addons\SharedMedia_MyMedia\sound\SwitchTanks.wav]]) 
+LSM:Register("sound", "TakeHealhtstoneIdiot", [[Interface\Addons\SharedMedia_MyMedia\sound\TakeHealhtstoneIdiot.mp3]]) 
+LSM:Register("sound", "TargetSwitch", [[Interface\Addons\SharedMedia_MyMedia\sound\TargetSwitch.wav]]) 
+LSM:Register("sound", "TargetSwitch_Soon", [[Interface\Addons\SharedMedia_MyMedia\sound\TargetSwitch_Soon.wav]]) 
+LSM:Register("sound", "TotsOnYou", [[Interface\Addons\SharedMedia_MyMedia\sound\TotsOnYou.wav]]) 
+LSM:Register("sound", "tyagi", [[Interface\Addons\SharedMedia_MyMedia\sound\tyagi.mp3]]) 
+LSM:Register("sound", "UseDefensive", [[Interface\Addons\SharedMedia_MyMedia\sound\UseDefensive.wav]]) 
+LSM:Register("sound", "UseHunterMark", [[Interface\Addons\SharedMedia_MyMedia\sound\UseHunterMark.mp3]]) 
+LSM:Register("sound", "VortexCast", [[Interface\Addons\SharedMedia_MyMedia\sound\VortexCast.wav]]) 
+LSM:Register("sound", "WatchMarks", [[Interface\Addons\SharedMedia_MyMedia\sound\WatchMarks.wav]]) 
+LSM:Register("sound", "WoW-Ony_50DKP", [[Interface\Addons\SharedMedia_MyMedia\sound\WoW-Ony_50DKP.ogg]]) 
+LSM:Register("sound", "WoW-Ony_DPS-Slow", [[Interface\Addons\SharedMedia_MyMedia\sound\WoW-Ony_DPS-Slow.ogg]]) 
+LSM:Register("sound", "WoW-Ony_FUCK", [[Interface\Addons\SharedMedia_MyMedia\sound\WoW-Ony_FUCK.ogg]]) 
+LSM:Register("sound", "WoW-Ony_WTF_01", [[Interface\Addons\SharedMedia_MyMedia\sound\WoW-Ony_WTF_01.ogg]]) 
+LSM:Register("sound", "WoW-Ony_WTF_02", [[Interface\Addons\SharedMedia_MyMedia\sound\WoW-Ony_WTF_02.ogg]]) 
+LSM:Register("sound", "WoW-Ony_WTF_03", [[Interface\Addons\SharedMedia_MyMedia\sound\WoW-Ony_WTF_03.ogg]]) 
+LSM:Register("sound", "WrongActionBar", [[Interface\Addons\SharedMedia_MyMedia\sound\WrongActionBar.mp3]]) 
+LSM:Register("sound", "YouAreNext", [[Interface\Addons\SharedMedia_MyMedia\sound\YouAreNext.wav]]) 
+
+-- -----
+--   STATUSBAR
+-- -----
+LSM:Register("statusbar", "Arrow9", [[Interface\Addons\SharedMedia_MyMedia\statusbar\Arrow9.tga]]) 
+LSM:Register("statusbar", "Flatt", [[Interface\Addons\SharedMedia_MyMedia\statusbar\Flatt.blp]]) 
+LSM:Register("statusbar", "glowTex", [[Interface\Addons\SharedMedia_MyMedia\statusbar\glowTex.tga]]) 
+LSM:Register("statusbar", "Minimalist", [[Interface\Addons\SharedMedia_MyMedia\statusbar\Minimalist.tga]]) 
+LSM:Register("statusbar", "normTex", [[Interface\Addons\SharedMedia_MyMedia\statusbar\normTex.tga]]) 
+LSM:Register("statusbar", "normTex2", [[Interface\Addons\SharedMedia_MyMedia\statusbar\normTex2.tga]]) 
+LSM:Register("statusbar", "Smooth", [[Interface\Addons\SharedMedia_MyMedia\statusbar\Smooth.tga]]) 
